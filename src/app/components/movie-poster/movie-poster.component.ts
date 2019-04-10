0a6866cdb97b6f2094a81a24673686e4b34c17e9
Johanna <josjlin@gmail.com>
2019-04-10 13:22:15 +0200

    add movie-poster component

---
+++ src/app/components/movie-poster/movie-poster.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-movie-poster",
+  templateUrl: "./movie-poster.component.html",
+  styleUrls: ["./movie-poster.component.css"]
+})
+export class MoviePosterComponent implements OnInit {
+  @Input() moviePoster: string;
+  constructor() {}
+
+  ngOnInit() {}
+}
