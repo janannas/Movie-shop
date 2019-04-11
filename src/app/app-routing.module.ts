80cae3127fbcffbb0ccb27379b42b5043ab67341
Johanna <josjlin@gmail.com>
2019-04-11 15:31:10 +0200

    add routingpaths, navbar, components for cart, catalog, home, not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component";
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+//Components
+import { HomeComponent } from "./components/home/home.component";
+import { CatalogComponent } from "./components/catalog/catalog.component";
+import { ShoppingCartComponent } from "./components/cart/cart.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+
+const routes: Routes = [
+  { path: "catalog", component: CatalogComponent },
+  { path: "shoppingCart", component: ShoppingCartComponent },
+  { path: "", component: HomeComponent },
+  { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   exports: [RouterModule],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { MoviePosterComponent } from "./components/movie-poster/movie-poster.component";
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./components/home/home.component";
+import { CatalogComponent } from "./components/catalog/catalog.component";
+import { ShoppingCartComponent } from "./components/cart/cart.component";
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviePosterComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    MoviePosterComponent,
+    HomeComponent,
+    CatalogComponent,
+    ShoppingCartComponent,
+    NotFoundComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
