40e1b05a9d46208d95f7a0187fed8d1c0d7a58bb
Johanna <josjlin@gmail.com>
2019-04-21 20:11:58 +0200

    cleaning up details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { MovieService } from "../../services/movie.service";
 import { IMovie } from "../../interfaces/IMovie";
@@ -11,34 +11,22 @@ import { IMovie } from "../../interfaces/IMovie";
 export class DetailsComponent implements OnInit {
   movies: IMovie[];
   movie: IMovie;
-  errorMsg: string;
 
   constructor(private route: ActivatedRoute, private service: MovieService) {
-    this.service.getMovieData().subscribe(
-      myData => {
-        this.movies = myData;
-
-        console.log(this.movie, this.movies);
-
-        this.route.paramMap.subscribe(myParams => {
-          let id = myParams.get("id");
-          console.log(id);
-          this.searchMovies(+id);
-        });
-      },
-      error => {
-        this.errorMsg = error;
-      }
-    );
+    this.service.getMovieData().subscribe(myData => {
+      this.movies = myData;
+      this.route.paramMap.subscribe(myParams => {
+        let id = myParams.get("id");
+        this.searchMovies(+id);
+      });
+    });
   }
-  searchMovies(myId: number): IMovie {
-    console.log(myId, this.movies);
+
+  searchMovies(myId: number): void {
     for (let i = 0; i < this.movies.length; i++) {
       const elem = this.movies[i];
       if (myId == elem.id) {
-        console.log(elem);
         this.movie = elem;
-        return elem;
       }
     }
   }
