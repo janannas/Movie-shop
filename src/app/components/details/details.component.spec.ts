a7c554c3ca98788b6745946ebeb22e82360371ea
Johanna <josjlin@gmail.com>
2019-04-23 13:14:02 +0200

    tests for details component

--- src/app/components/details/details.component.spec.ts
+++ src/app/components/details/details.component.spec.ts
@@ -33,11 +33,37 @@ describe("DetailsComponent", () => {
     expect(component).toBeTruthy();
   });
 
+  it("constructor should get instances of IMovie", () => {
+    expect(component.movies).toContain({
+      id: 76,
+      name: "The Dark Knight",
+      description:
+        "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice",
+      price: 199,
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+      year: 2008,
+      added: "2016-01-05T00:00:00",
+      productCategory: [
+        {
+          categoryId: 5,
+          category: null
+        },
+        {
+          categoryId: 6,
+          category: null
+        }
+      ]
+    });
+  });
+
   it("movies should have the length of 3", () => {
     expect(component.movies.length).toBe(3);
   });
 
   it("searchMovies should return the the correct id", () => {
-    //Testa att logiken är rätt, att jag får tillbaka det id:et jag förväntar mig
+    expect(component.movie).toBeUndefined();
+    component.searchMovies("77");
+    expect(component.movie.id).toBe(77);
   });
 });
