1afa797d273413aab2b28fb56b422883add01fed
Johanna <josjlin@gmail.com>
2019-04-24 13:32:30 +0200

    products stored in isolated cartArray

--- src/app/components/movie-poster/movie-poster.component.ts
+++ src/app/components/movie-poster/movie-poster.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { IMovie } from "src/app/interfaces/IMovie";
 
 @Component({
   selector: "app-movie-poster",
@@ -6,9 +7,16 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./movie-poster.component.css"]
 })
 export class MoviePosterComponent implements OnInit {
+  @Input() movie: IMovie;
   @Input() moviePoster: string;
   @Input() movieId: number;
+  cart: IMovie[] = [];
   constructor() {}
 
   ngOnInit() {}
+
+  addToCart(product: IMovie): void {
+    this.cart.push(product);
+    console.log(this.cart);
+  }
 }
