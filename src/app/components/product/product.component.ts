505d0bd1bd2ebdb71b5017bf9d4f651aa0f62975
Johanna <josjlin@gmail.com>
2019-04-29 22:14:22 +0200

    Revert "remove @Input MovieId from productComponent"
    This reverts commit ec980963f4a25e81df8a173f26dd9e517444f301.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -10,6 +10,7 @@ import { CartService } from "src/app/services/cart.service";
 export class ProductComponent implements OnInit {
   @Input() movie: IMovie;
   @Input() moviePoster: string;
+  @Input() movieId: number;
 
   constructor(private cartService: CartService) {}
 
