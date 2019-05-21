a5b8354864d5c7e2a4aa5b05f15406ccd74dbce1
Johanna <josjlin@gmail.com>
2019-05-21 13:42:30 +0200

    test connectCategoriesToMovie

--- src/app/components/catalog/catalog.component.spec.ts
+++ src/app/components/catalog/catalog.component.spec.ts
@@ -7,6 +7,7 @@ import { MockMovieService } from "../../services/mock-movie.service";
 import { CatalogComponent } from "./catalog.component";
 import { ProductComponent } from "../product/product.component";
 import { ErrorComponent } from "../error/error.component";
+import { connect } from 'net';
 
 describe("CatalogComponent", () => {
   let component: CatalogComponent;
@@ -41,6 +42,39 @@ describe("CatalogComponent", () => {
   it("movies should have the length of 3", () => {
     expect(component.movies.length).toBe(3);
   });
+
+  it("should connect correct category to movie", () => {
+    const mockCategory = [
+      {
+        id: 7,
+        name: "MockCategory"
+      }
+    ];
+    const mockProduct = [
+      {
+        id: 78,
+        name: "Le fabuleux destin d'Amélie Poulain",
+        description:
+          "Amélie is an innocent and naive girl in Paris with her own sense of justice. She decides to help those around her and, along the way, discovers love.",
+        price: 100,
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/M/MV5BNDg4NjM1YjMtYmNhZC00MjM0LWFiZmYtNGY1YjA3MzZmODc5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+        year: 2001,
+        added: "2017-07-10T00:00:00",
+        productCategory: [
+          {
+            categoryId: 7,
+            category: null
+          }
+        ]
+      }
+    ];
+
+    component.movies = mockProduct;
+
+    component.connectCategoriesToMovie(mockCategory);
+    expect(mockProduct[0].productCategory[0].categoryId).toBe(7);
+  })
 
   it("should display correct category", () => {
     const testMovieCategory = component.movies[1].productCategory[0].category;
