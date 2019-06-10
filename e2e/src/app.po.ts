eed83d8dd65921ca359398c7c27fdc00aa15da01
Johanna <josjlin@gmail.com>
2019-06-10 16:52:45 +0200

    add e2e tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,35 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from "protractor";
 
 export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  navigateToRoute(path: string) {
+    return browser.get(path);
+  }
+
+  getPageTitle() {
+    return element(by.css("h1")).getText() as Promise<string>;
+  }
+
+  getNavLink() {
+    return element(by.css("nav a")).getText() as Promise<string>;
+  }
+
+  getProductTitle() {
+    return element(by.className("label"))
+      .element(by.css("h2"))
+      .getText() as Promise<string>;
+  }
+
+  getToggleCategoriesButton() {
+    return element(by.className("categories-container")).element(
+      by.css("button")
+    ) as ElementFinder;
+  }
+
+  getListElement() {
+    return element(by.css("ul")).element(by.css("li")) as ElementFinder;
   }
 }
